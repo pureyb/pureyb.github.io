00c79b9fc5fe7d17aeeb3256815e01d89b63dcb7
bo.yang <bo.yang@upai.com>
2020-06-22 11:56:37 +0800

    update

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -1,5 +1,5 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
-// Updated by http://oidbo.com
+// Updated by http://https://pureyb.github.io/
 (function($){
   $.fn.toc = function(options) {
     var defaults = {
